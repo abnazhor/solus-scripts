f68c5036b5326244768eda576545a66d1989af56
abnazhor <jorbar982@outlook.com>
2020-10-27 16:37:00 +0100

    Name update. Add minecraft and zerotier code

---
+++ programs/minecraft.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#Installation script for Typora - Abnazhor
+#https://github.com/abnazhor/solus-scripts
+
+#Imports custom logger functions to display information
+source ../utils/logger.sh
+
+cd ~/.builds || mkdir ~/.builds && cd ~/.builds;
+info "Removing old Minecraft Launcher files if exists...";
+rm -r minecraft-launcher;
+mkdir minecraft-launcher;
+cd minecraft-launcher;
+
+info "Downloading Minecraft Launcher from source... (https://launcher.mojang.com/download/)";
+wget --quiet https://launcher.mojang.com/download/Minecraft.tar.gz;
+success "Done.";
+
+info "Extracting...";
+tar -xf Minecraft.tar.gz;
+mv minecraft-launcher launcher;
+mv launcher/* .;
+success "Done.";
+
+info "Removing unnecesary files...";
+rmdir minecraft-launcher;
+rm -r Minecraft.tar.gz;
+success "Done.";
+
+info "Adding a desktop shortcut...";
+echo "[Desktop Entry]
+Encoding=UTF-8
+Version=1.0
+Type=Application
+Terminal=false
+Exec=/home/$USER/.builds/minecraft-launcher/minecraft-launcher
+Name=Minecraft Launcher" > ~/.local/share/applications/minecraft-launcher.desktop;
+
+success "Minecraft Launcher has been successfully installed.";
--- programs/typora.sh
+++ programs/typora.sh
@@ -6,12 +6,12 @@
 source ../utils/logger.sh
 
 cd ~/.builds || mkdir ~/.builds && cd ~/.builds;
-info "Removing old Typora files...";
+info "Removing old Typora files if exists...";
 rm -r typora;
 mkdir typora;
 cd typora;
 
-info "Downloading Typora from source...";
+info "Downloading Typora from source... (https://typora.io/)";
 wget --quiet https://typora.io/linux/Typora-linux-x64.tar.gz;
 success "Done.";
 
@@ -25,7 +25,7 @@ rm -r bin
 rm -r Typora-linux-x64.tar.gz;
 success "Done.";
 
-info "Adding a desktop shortcut";
+info "Adding a desktop shortcut...";
 echo "[Desktop Entry]
 Encoding=UTF-8
 Version=1.0
@@ -34,3 +34,4 @@ Terminal=false
 Exec=/home/$USER/.builds/typora/Typora
 Name=Typora" > ~/.local/share/applications/typora.desktop
 
+success "Typora has been successfully installed.";
